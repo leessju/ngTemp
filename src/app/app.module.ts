881fe690ea86aeab9ce70e48d4af0df59ea890c5
nicejames <leessju@gmail.com>
2018-06-19 14:07:12 +0900

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BulmaComponent } from './components/bulma/bulma.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BulmaComponent
   ],
   imports: [
     BrowserModule
